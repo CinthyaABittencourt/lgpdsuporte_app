ab45108c6ae0753f21bb0515f85d7a2e32c54ba9
Cinthya Bittencourt <cinthya.bittencourt@ufrpe.br>
2021-04-03 23:35:04 -0300

    Tela Inicial

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,54 @@
 import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+        @override
+        Widget build(BuildContext context) {
+                return MaterialApp(
+                    title: 'Welcome to Flutter',
+                    home: Scaffold(
+                            appBar: AppBar(
+                                    title: const Text('LGPD Suporte'),
+                            ),
+                            body: Center(
+                                    child: Column(
+                                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                            children: [
+                                                    Text('Seja Bem-Vindo!',
+                                                            style: TextStyle(
+                                                                    fontSize: 30,
+                                                                    color: Colors.blueAccent,
+                                                            ),
+                                                    ),
+                                                    ConstrainedBox(
+                                                            constraints: BoxConstraints.tightFor(width: 300, height: 100),
+                                                            child: ElevatedButton(
+                                                                    child: Text('CONTROLADORES'),
+                                                                    onPressed: () {},
+                                                                    style: ButtonStyle(
+                                                                        backgroundColor: MaterialStateProperty.all(Colors.blueAccent),
+                                                                        padding: MaterialStateProperty.all(EdgeInsets.all(28)),
+                                                                        textStyle: MaterialStateProperty.all(TextStyle(fontSize: 28))),
+                                                            ),
+                                                    ),
+                                                    ConstrainedBox(
+                                                            constraints: BoxConstraints.tightFor(width: 300, height: 100),
+                                                            child: ElevatedButton(
+                                                                    child: Text('USUÁRIOS'),
+                                                                    onPressed: () {},
+                                                                    style: ButtonStyle(
+                                                                        backgroundColor: MaterialStateProperty.all(Colors.blueAccent),
+                                                                        padding: MaterialStateProperty.all(EdgeInsets.all(28)),
+                                                                        textStyle: MaterialStateProperty.all(TextStyle(fontSize: 28))),
+                                                            ),
+                                                    ),
+                                            ],
+                                    ),
+                            ),
+                    )
+                );
+        }
+}
+
+
